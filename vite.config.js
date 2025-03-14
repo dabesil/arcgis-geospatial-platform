6452a05faca1bd869c846a1a9a602e62272e7eb0
dabesil <daviibessa@gmail.com>
2025-03-14 09:57:40 -0300

    Ajuste config para deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dynamicImport from 'vite-plugin-dynamic-import'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,6 +18,7 @@ export default defineConfig({
       },
     }),
     vueDevTools(),
+    dynamicImport(),
   ],
   resolve: {
     alias: {
