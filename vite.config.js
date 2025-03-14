bda5605d036cb6c770685df87a02b12e76254d6a
dabesil <daviibessa@gmail.com>
2025-03-14 09:47:42 -0300

    Ajustes nas configurações de build

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['vue', 'vue-router', 'vuex'],
+          vendor: ['vue', 'vuex'],
         },
       },
     },
